82970147a998439bfcc0ca019638c39b735b7ee5
shanuwa2 <162961561+shanuwa2@users.noreply.github.com>
2024-10-24 18:35:34 +0530

    Update config.js
    Signed-off-by: shanuwa2 <162961561+shanuwa2@users.noreply.github.com>

--- config.js
+++ config.js
@@ -5,6 +5,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=Xn5nSSCa#1NTkK_yqY0kimSNm2NXaDRuHkrsvWr9eSSxLPmYrTHI",
+SESSION_ID: process.env.SESSION_ID || "𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=b6IwXAbQ#q9pjzrLexSGA-qlxXHVeG7_llGgb97p6jbX_fhaKc8E",
 MONGODB: process.env.MONGODB || "mongodb://mongo:ATUdNXYlCCXxOvdqUlXjWObNPwdkdpib@autorack.proxy.rlwy.net:42669",//enter mongo db url හදන විදිය පල්ලෙහාබටන් එකක් ඇති
 };
